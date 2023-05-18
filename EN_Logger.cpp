c0a5b8058c9929ec4db1782e49ec6f0464a9246e
mrognor <timurlego1@yandex.ru>
2023-05-18 11:26:54 +0300

    Make log function thread safety

--- EN_Logger.cpp
+++ EN_Logger.cpp
@@ -7,6 +7,8 @@ namespace EN
 
     void DefaultLogFunc(logLevels logLevel, std::string logMessage)
     {
+        static std::mutex mtx;
+
         if (MaxLogLevel == -1)
             MaxLogLevel = Message;
 
@@ -15,27 +17,32 @@ namespace EN
         case Message:
             if (MaxLogLevel > 0)
                 break;
+            mtx.lock();
             std::cerr << GetCurrentDayTime() << " [message] " << logMessage << std::endl;
             break;
 
         case Hint:
             if (MaxLogLevel > 0)
                 break;
+            mtx.lock();
             std::cerr << "\x1B[94m" << GetCurrentDayTime() << " [hint] " << logMessage << "\033[0m" << std::endl;
             break;
 
         case Warning:
             if (MaxLogLevel > 1)
                 break;
+            mtx.lock();
             std::cerr << "\x1B[33m" << GetCurrentDayTime() << " [warning] " << logMessage << "\033[0m" << std::endl;
             break;
 
         case Error:
             if (MaxLogLevel > 2)
                 break;
+            mtx.lock();
             std::cerr << "\x1B[31m" << GetCurrentDayTime() << " [error] " << logMessage << "\033[0m" << std::endl;
             break;
         }
+        mtx.unlock();
     }
 
     void SetLogFunc(void (*logFunc)(logLevels, std::string), logLevels maxLogLevel)
