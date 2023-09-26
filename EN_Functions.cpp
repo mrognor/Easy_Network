729c9f8b91000860c2f6af38cc3fbfd68f04e904
Timur Ammaev <timurlego1@yandex.ru>
2023-09-26 16:25:31 +0300

    Add function to generate uui4

--- EN_Functions.cpp
+++ EN_Functions.cpp
@@ -1028,4 +1028,37 @@ namespace EN
 				+ std::to_string(optionValue)
 				+ " Error code: " + std::to_string(GetSocketErrorCode()) + " " + EN::GetSocketErrorString());
 	}
+
+	std::string UUID4()
+    {
+        std::string res;
+        std::random_device rd;
+        std::uniform_int_distribution<int> dist(0, 15);
+
+        char chars[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+        for (int i = 0; i < 32; i++)
+        {
+            int a = dist(rd);
+
+            if (i == 12)
+            {
+            res += std::to_string(0b0100);
+            continue;
+            }
+
+            if (i == 16)
+            {
+                a |= 0b1000;
+                a &= ~(0b0100);
+            }
+
+            res += chars[a];
+
+            if (i == 7 || i == 11 || i == 15 || i == 19)
+                res += "-";
+        }
+
+        return res;
+    }
 }
